c68422c27c6eb883e92ca28221da74137554c879
Mitchell Lisle <lislemitchell@gmail.com>
2021-11-12 12:20:44 +1100

    bump version

--- siphon/__init__.py
+++ siphon/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Mitchell Lisle"""
 __email__ = 'm.lisle90@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 from siphon.database.mysql import AioMySQL, MySQLConfig  # noqa: F401
