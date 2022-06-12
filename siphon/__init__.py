67604b702decd21de7dc919d8e5e019f05823867
Mitchell Lisle <lislemitchell@gmail.com>
2022-06-12 11:59:22 +1000

    updates

--- siphon/__init__.py
+++ siphon/__init__.py
@@ -5,21 +5,20 @@ __email__ = 'm.lisle90@gmail.com'
 __version__ = '1.0.0'
 
 
-from siphon.database.aiopostgres import AioPostgres, Record  # noqa: F401
+from siphon.database.aiopostgres.client import AioPostgres  # noqa: F401
+from siphon.database.aiopostgres.client import Record  # noqa: F401
 from siphon.database.bigquery import (BigQuery, BigQueryConfig,  # noqa: F401
                                       Row)
-from siphon.database.mysql import AioMySQL, MySQLConfig  # noqa: F401
-from siphon.database.postgres import Postgres, PostgresConfig  # noqa: F401
-from siphon.database.aiopostgres.client import AioPostgres, Record  # noqa: F401
-from siphon.database.config import PostgresConfig, DatabaseConfig, MySQLConfig  # noqa: F401
+from siphon.database.config import (DatabaseConfig, MySQLConfig,  # noqa: F401
+                                    PostgresConfig)
 from siphon.database.mysql import AioMySQL  # noqa: F401
 from siphon.database.postgres import Postgres  # noqa: F401
 from siphon.logger import logger  # noqa: F401
 from siphon.queue.aioqueue import CollectedError  # noqa: F401
 from siphon.queue.aioqueue import (AioQueue, TypedAioQueue,  # noqa: F401
                                    queuecollect)
+from siphon.queue.violations import DiscardOnViolation  # noqa: F401
 from siphon.queue.violations import RaiseOnViolation  # noqa: F401
-from siphon.queue.violations import (DiscardOnViolation,  # noqa: F401
-                                     ViolationStrategy)
-from siphon.rabbit.aiorabbit import (AioRabbitConsumer,  # noqa: F401
-                                     RabbitConfig)
+from siphon.queue.violations import ViolationStrategy  # noqa: F401
+from siphon.rabbit.aiorabbit import AioRabbitConsumer  # noqa: F401
+from siphon.rabbit.aiorabbit import RabbitConfig  # noqa: F401
